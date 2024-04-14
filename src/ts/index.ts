3fd1e268d18e71a48a700a22bbd847b42cb18fa5
shreyas <shreyu.raju@gmail.com>
2024-04-14 15:14:29 +0530

    Still need to work on perfect audio play

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,23 +1,31 @@
 import { notesToPlayInOrder } from "./music-to-play";
 import { MusicalNote, BEATS_PER_MINUTE } from "./musical-score";
 
+//function to get audio id
 function getAudioId(note: MusicalNote): HTMLAudioElement {
     let id = note.pitch + note.octave.toString();
+    //adding the extra pitch if it exist
     if (note.accidental) {
         id += note.accidental;
     }
     console.log(id);
+//returning the audio element which is already exist in index.html
     return document.getElementById(id) as HTMLAudioElement;
 }
 
+//function to play audio using notes
 function playAudio(note: MusicalNote) {
+    //call back function get Audio ID 
     const audioId = getAudioId(note);
     // console.log(audioId);
     if (audioId) {
+        //playing audio
         audioId.play();
         setTimeout(() => {
+            //stopping audio and reseting play time to 0
             audioId.pause()
             audioId.currentTime = 0;
+            //use of note.beats to play the duration of audio
         }, note.beats * (60000 / BEATS_PER_MINUTE));
     } else {
         console.log(`Audio element not found or ${note}`);
