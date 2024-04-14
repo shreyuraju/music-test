7aea7707b14d193fa0d71387f9be7fe9bd0974d6
shreyas <shreyu.raju@gmail.com>
2024-04-14 13:15:10 +0530

    need to set perfect timming still working on it

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,40 @@
 import { notesToPlayInOrder } from "./music-to-play";
+import { MusicalNote, BEATS_PER_MINUTE } from "./musical-score";
+
+function getAudioId(note: MusicalNote): HTMLAudioElement {
+    let id = note.pitch + note.octave.toString();
+    if (note.accidental) {
+        id += note.accidental;
+    }
+    console.log(id);
+    return document.getElementById(id) as HTMLAudioElement;
+}
+
+function playAudio(note: MusicalNote) {
+    const audioId = getAudioId(note);
+    // console.log(audioId);
+    if (audioId) {
+        audioId.play();
+        setTimeout(() => {
+            audioId.pause()
+            audioId.currentTime = 0;
+        }, note.beats * (60000 / BEATS_PER_MINUTE));
+    } else {
+        console.log(`Audio element not found or ${note}`);
+    }
+}
 
 function playMusic() {
     const notes = notesToPlayInOrder;
-    
+
     // TODO Play these notes one after the other at the pitch and rhythm given in each note
+    let time: number = 0;
+    notes.forEach(note => {
+        setTimeout(() => {
+            playAudio(note)
+        }, time * (60000 / BEATS_PER_MINUTE));
+        time += note.beats
+    });
 }
 
 document.getElementById('start-playing')?.addEventListener('click', playMusic);
